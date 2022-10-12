768b7064068b193c0884bb1dc9133d9eea24453d
Aki9C <Aki9c223@gmail.com>
2022-10-12 17:01:57 +0900

    commit

--- topPage/prod/memo/04_memo/js/memo.js
+++ topPage/prod/memo/04_memo/js/memo.js
@@ -6,6 +6,7 @@ function(){
         window.alert("このブラウザはLocal Storage機能が実装されていないです。");
         return;
     }else{
+        viewStorage();
         saveLocalStorage();
     }
 },false
@@ -23,6 +24,7 @@ function saveLocalStorage(){
             return;
         }else{
             localStorage.setItem(key, value);
+            viewStorage();
             let w_msg="LocalStorageに"+key+" "+value+"を保存しました。";
             window.alert(w_msg);
             document.getElementById("textKey").value="";
@@ -30,4 +32,23 @@ function saveLocalStorage(){
         }
     },false
     )
+}
+
+function viewStorage(){
+    const list=document.getElementById("list");
+    while( list.rows[0] )list.deleteRow(0);
+    for(let i=0;i<localStorage.length;i++){
+        let w_key=localStorage.key(i);
+        const tr=document.createElement("tr");
+        const td1=document.createElement("td");
+        const td2=document.createElement("td");
+        const td3=document.createElement("td");
+        list.append(tr);
+        tr.append(td1);
+        tr.append(td2);
+        tr.append(td3);
+        td1.innerHTML="<input name='radio1' type='radio'>";
+        td2.innerHTML=w_key;
+        td3.innerHTML=localStorage.getItem(w_key);
+    }
 }
