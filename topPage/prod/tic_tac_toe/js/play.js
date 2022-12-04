983cecd9b85838cdc0a2b940f6ad61f86ed8488d
Aki9C <Aki9c223@gmail.com>
2022-12-05 00:30:52 +0900

    commited

---
+++ topPage/prod/tic_tac_toe/js/play.js
@@ -0,0 +1,211 @@
+
+let stateSet = document.querySelector(".state_set");
+let musicHead = document.querySelector(".head");
+let musicOBJ = document.querySelector("audio");
+let proLine = document.querySelector(".pros");
+let timeshow = document.querySelector(".time_show");
+let musicLineMoveFa = document.querySelector(".progress");
+let musicLineMove = document.querySelector(".pros");
+let volumeBtn = document.querySelector(".volume");
+let volumeBar = document.querySelector(".volume_range");
+let volumePro = document.querySelector(".range");
+let musicLast = document.querySelector(".last");
+let musicNext = document.querySelector(".next");
+let musicBOXbc = document.querySelector(".play_bc > img");
+let musicInfoShow = document.querySelector(".music_info");
+
+
+window.onload = function(){
+    musicPlaySet();
+    musicMove();
+    musicListen();
+    volumeSet();
+    musicJump();
+}
+
+let time = null;
+let time2 = null;
+let time3 = null;
+let time4 = null;
+let f1 = false;
+let f2 = true;
+let i1 = 0;
+let nowtime = 0;
+let currtime = 0;
+musicOBJ.loop=true;
+
+function musicPlaySet(){
+    stateSet.onclick = function(){
+        if(f1){
+            musicStop();
+        }else{
+            stateSet.className = "state_set iconfont icon-24gf-pause2";
+            musicOBJ.play();
+            musicTime();
+            time = setInterval(() => {
+                i1++;
+                musicHead.style.transform = `rotate(${i1}deg)`;
+            }, 20);
+            f1 = true;
+        }
+    }
+}
+
+function musicTime(){
+    let time = Math.floor(musicOBJ.duration);
+    let minute = Math.floor(time / 60) + "";
+    minute = minute < 10 ? "0" + minute : minute; 
+    let second = time % 60 + "";
+    second = second < 10 ? "0" + second : second; 
+    time4 = setInterval(() => {
+        let muscurrtime = musicOBJ.currentTime;
+        let currminute = Math.floor(muscurrtime / 60) + "";
+        currminute = currminute < 10 ? "0" + currminute : currminute; 
+        let currsecond = Math.floor(muscurrtime % 60) + "";
+        currsecond = currsecond < 10 ? "0" + currsecond : currsecond; 
+        timeshow.innerText = `${currminute}:${currsecond} / ${minute}:${second}`;
+        if(currminute==minute&&currsecond==second){
+            proLine.style.width = "0px";
+            nowtime = 0;
+            clearInterval(time);
+            clearInterval(time2);
+            clearInterval(time3);
+            clearInterval(time4);
+        } 
+    }, 1);
+    time2 = setInterval(() => {
+        let movetime = 330 / time;
+        nowtime += movetime;
+        proLine.style.width = nowtime + "px";
+    }, 1000);
+}
+
+function musicMove(){
+    musicLineMove.onmousedown = function(){
+        clearInterval(time);
+        clearInterval(time2);
+        clearInterval(time4);
+        musicLineMoveFa.onmousemove = function(){
+            clearInterval(time);
+            clearInterval(time2);
+            clearInterval(time4);
+            x = event.clientX - (window.innerWidth - 330) / 2 - 30;
+            proLine.style.width = x + "px";
+            currtime = x / (330 / musicOBJ.duration);
+        }
+    }
+    musicLineMove.onmouseup = function(){
+        musicLineMoveFa.onmousemove = null;
+        musicOBJ.currentTime = currtime;
+        proLine.style.width = currtime * (330 / musicOBJ.duration) + "px";
+        nowtime = currtime * (330 / musicOBJ.duration);
+        musicTime();
+        musicOBJ.play();
+        stateSet.className = "state_set iconfont icon-24gf-pause2";
+        time = setInterval(() => {
+            i1++;
+            musicHead.style.transform = `rotate(${i1}deg)`;
+        }, 20);
+        f1 = true;
+    }
+}
+
+function musicListen(){
+    time3 = setInterval(() => {
+        if(musicOBJ.ended){
+            proLine.style.width = "0px";
+            stateSet.className = "state_set iconfont icon-bofang";
+            nowtime = 0;
+            clearInterval(time);
+            clearInterval(time2);
+            clearInterval(time3);
+            clearInterval(time4);
+            f1 = false;
+        }
+    }, 1000);
+}
+
+function volumeSet(){
+    volumePro.autofocus = true;
+    volumePro.defaultValue = 100;
+    volumePro.step = 1;
+    volumePro.max = 100;
+    volumePro.min = 0;
+    volumeBtn.onmouseenter = function(){
+        volumeBar.style.height = "100px";
+        volumeBar.style.padding = "5px";
+        volumeBar.style.top = "-110px";
+        volumeBtn.onclick = function(){
+            if(f2){
+                volumePro.disabled = true;
+                volumeBtn.className = "volume iconfont icon-volumeDisable";
+                musicOBJ.muted = true;
+                f2 = false;
+            }else{
+                volumePro.disabled = false;
+                volumeBtn.className = "volume iconfont icon-volumeMiddle";
+                musicOBJ.muted = false;
+                f2 = true;
+            }
+        }
+    }
+    volumeBtn.onmouseleave = function(){
+        volumeBar.style.height = "0px";
+        volumeBar.style.padding = "0px";
+        volumeBar.style.top = "0px";
+    }
+    volumePro.onfocus = function(){
+        volumeBtn.onclick = null;
+        this.onchange = function(){
+            musicOBJ.volume = this.value / 100;
+            console.log(this.value);
+            if(this.value === 0){
+                volumeBtn.className = "volume iconfont icon-volumeDisable";
+                f2 = false;
+            }
+        }
+    }
+}
+
+let musicList = ["Reprologue","Shadyshady"];
+function musicJump(){
+    for (let i = 0; i < musicList.length; i++) {
+        musicLast.onclick = function(){
+            musicInfoShow.innerText = musicList[i];
+            musicOBJ.src = "music/" + musicList[i] + ".mp3";
+            musicBOXbc.src = "head/" + musicList[i] + ".jpg";
+            musicHead.src = "head/" + musicList[i] + ".jpg";
+            nowtime = 0;
+            console.log(i);
+            i--;
+            if(i < 0 || i > 5){
+                i = musicList.length - 1;
+            }
+            musicStop();
+        }
+        musicNext.onclick = function(){
+            musicInfoShow.innerText = musicList[i];
+            musicOBJ.src = "music/" + musicList[i] + ".mp3";
+            musicBOXbc.src = "head/" + musicList[i] + ".jpg";
+            musicHead.src = "head/" + musicList[i] + ".jpg";
+            nowtime = 0;
+            console.log(i);
+            i++;
+            if(i < 0 || i >= 1){
+                i = 0;
+            }
+            musicStop();
+        }
+    }
+}
+
+function musicStop(){
+    stateSet.className = "state_set iconfont icon-bofang";
+    musicOBJ.pause();
+    clearInterval(time);
+    clearInterval(time2);
+    clearInterval(time4);
+    f1 = false;
+}
+
+
--- topPage/prod/tic_tac_toe/js/tic_tac_toe.js
+++ topPage/prod/tic_tac_toe/js/tic_tac_toe.js
@@ -37,16 +37,16 @@ restart.addEventListener("click",()=>{
 
 function isSelect(e){
     if (flag===0){
+        new Audio(wSound[0]).play();
         e.classList.add("js-white-checked","js-unclickable");
         e.setAttribute('value',0);
-        new Audio(wSound[0]).play();
         setMessage("black-turn");
         flag=1
     }else if(flag===1){
+        new Audio(wSound[1]).play();
         e.classList.add("js-black-checked","js-unclickable");
         e.setAttribute('value',1);
         setMessage("white-turn");
-        new Audio(wSound[1]).play();
         flag=0;
     }
     counter-=1;
@@ -58,15 +58,15 @@ function setMessage(id){
     }else if(id==="black-turn"){
         msgtext.innerHTML=msgtxt2;
     }else if(id==="draw"){
-        msgtext.innerHTML="<p class=text>Drow</p>";
         new Audio(wSound[2]).play();
+        msgtext.innerHTML="<p class=text>Drow</p>";
     }else if(id==="white-win"){
-        msgtext.innerHTML="<p class=text1>White win!</p>";
         new Audio(wSound[3]).play();
+        msgtext.innerHTML="<p class=text1>White win!</p>";
         flag=2;
     }else if(id==="black-win"){
-        msgtext.innerHTML="<p class=text2>Black win!</p>"
         new Audio(wSound[4]).play();
+        msgtext.innerHTML="<p class=text2>Black win!</p>"
     }
 }
 
