207d4db71024a146dd144c464aac62db86a807dc
Aki9C <Aki9c223@gmail.com>
2022-12-14 05:13:14 +0900

    commit

--- topPage/prod/tic_tac_toe/js/tic_tac_toe.js
+++ topPage/prod/tic_tac_toe/js/tic_tac_toe.js
@@ -1,18 +1,22 @@
 "use strict";
+let n;
 let flag=0;
 let counter=9;
 let wSound=['sound/click_sound1.mp3','sound/click_sound2.mp3','sound/draw_sound.mp3','sound/white_sound.mp3','sound/black_sound.mp3'];
 let value=new Array(9);
 let whiteStorage=new Array();
 let blackStorage=new Array();
+let randomStorage=[0,1,2,3,4,5,6,7,8];
 const square=document.getElementsByClassName("square");
 const restart=document.getElementById("restart");
 const menu=document.getElementById("menu");
 const msgtxt1='<p class="image"><img src="img/white.png" width=61px height=61px></p><p class=text>White Attack!</p>';
 const msgtxt2='<p class="image"><img src="img/black.png" width=61px height=61px></p><p class=text>Black Attack!</p>';
+const msgtxt3='<p class="image"><img src="img/white.png" width=61px height=61px></p><p class=text1>White Win!</p>';
+const msgtxt4='<p class="image"><img src="img/black.png" width=61px height=61px></p><p class=text2>Black Win!</p>';
+const msgtxt0='<p class="text"style="height:103px; font-size:48px;">Drow!</p>';
 const winCombos=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]];
 const image=['img/ParticleSmoke.png','img/star.png'];
-
 
 window.addEventListener("DOMContentLoaded",
     function() {
@@ -20,11 +24,11 @@ window.addEventListener("DOMContentLoaded",
     }, false
 );
 
-
 for(let i=0;i<square.length;i++){
     square[i].addEventListener("click",()=>{
         isSelect(square[i]);
         valueStorage(square,i);
+        computer(1);
         checkWin();
         if(counter==0&&flag!=2){
             setMessage("draw");
@@ -46,25 +50,25 @@ restart.addEventListener('click',e=>{
     },1000)
 })
 
-
-
-
 function isSelect(e){
     if (flag===0){
         new Audio(wSound[0]).play();
         e.classList.add("js-white-checked","js-unclickable");
         e.setAttribute('value',0);
         setMessage("black-turn");
+        
         flag=1
     }else if(flag===1){
-        new Audio(wSound[1]).play();
+        /*new Audio(wSound[1]).play();
         e.classList.add("js-black-checked","js-unclickable");
         e.setAttribute('value',1);
-        setMessage("white-turn");
+        setMessage("white-turn");*/
+        
         flag=0;
     }
     counter-=1;
 }
+
 function setMessage(id){
     const msgtext=document.getElementById("msgtext");
     if(id==="white-turn"){
@@ -73,14 +77,14 @@ function setMessage(id){
         msgtext.innerHTML=msgtxt2;
     }else if(id==="draw"){
         new Audio(wSound[2]).play();
-        msgtext.innerHTML="<p class=text>Drow</p>";
+        msgtext.innerHTML=msgtxt0;
     }else if(id==="white-win"){
         new Audio(wSound[3]).play();
-        msgtext.innerHTML="<p class=text1>White win!</p>";
+        msgtext.innerHTML=msgtxt3;
         flag=2;
     }else if(id==="black-win"){
         new Audio(wSound[4]).play();
-        msgtext.innerHTML="<p class=text2>Black win!</p>"
+        msgtext.innerHTML=msgtxt4;
     }
 }
 
@@ -93,9 +97,11 @@ function newGame(){
     value=new Array(9);
     whiteStorage=new Array();
     blackStorage=new Array();
+    randomStorage=[0,1,2,3,4,5,6,7,8];
     setMessage("white-turn");
     $(document).snowfall("clear");
 }
+
 function valueStorage(square,i){
     value[i]=square[i].getAttribute('value');
     if(value[i]==0){
@@ -105,6 +111,7 @@ function valueStorage(square,i){
        blackStorage.push(i);
     }
 }
+
 function checkWin(){
     for(let [,win]of winCombos.entries()){
         if(win.every(Element=>whiteStorage.indexOf(Element)>-1)){
@@ -125,14 +132,33 @@ function gameOver(){
         square[i].classList.add("js-unclickable");
     }
 }
+
 function snowfall(n){
     $(document).ready(function(){
     $(document).snowfall({
-    maxSpeed :25, 
+    maxSpeed :10, 
     minSpeed :1,
-    maxSize :80,
+    maxSize :60,
     minSize :30, 
     image:image[n],
-    })})
+    })
+    })
+}
+
+function computer(mode){
+    if(mode==1){
+        for(let [,value]of whiteStorage.entries()){
+            randomStorage.splice(value,1);
+        }
+        for(let [,value]of blackStorage.entries()){
+            randomStorage.splice(value,1);
+        }
+        n=Math.floor(Math.random()*randomStorage.length);
+        new Audio(wSound[1]).play();
+        square[randomStorage[n]].classList.add("js-black-checked","js-unclickable");
+        square[randomStorage[n]].setAttribute('value',1);
+        setMessage("white-turn");
+        flag=0;
+    }
 }
 
