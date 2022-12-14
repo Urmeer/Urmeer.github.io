a39ad6aa51f6a505f1507826125d6590b68f02a0
Aki9C <Aki9c223@gmail.com>
2022-12-15 05:05:56 +0900

    commit

--- topPage/prod/tic_tac_toe/js/tic_tac_toe.js
+++ topPage/prod/tic_tac_toe/js/tic_tac_toe.js
@@ -1,12 +1,12 @@
 "use strict";
-let n;
-let flag=0;
+let flag=0; 
 let counter=9;
 let wSound=['sound/click_sound1.mp3','sound/click_sound2.mp3','sound/draw_sound.mp3','sound/white_sound.mp3','sound/black_sound.mp3'];
-let value=new Array(9);
+let valueStorage=new Array(9);
 let whiteStorage=new Array();
 let blackStorage=new Array();
-let randomStorage=[0,1,2,3,4,5,6,7,8];
+let lineStorage=new Array();
+let randomStorage=new Array();
 const square=document.getElementsByClassName("square");
 const restart=document.getElementById("restart");
 const menu=document.getElementById("menu");
@@ -26,12 +26,12 @@ window.addEventListener("DOMContentLoaded",
 
 for(let i=0;i<square.length;i++){
     square[i].addEventListener("click",()=>{
-        isSelect(square[i]);
+        isSelect(square[i],1);
         valueStorage(square,i);
-        computer(1);
         checkWin();
         if(counter==0&&flag!=2){
             setMessage("draw");
+            gameOver("draw");
         }
     })
 }
@@ -50,23 +50,30 @@ restart.addEventListener('click',e=>{
     },1000)
 })
 
-function isSelect(e){
-    if (flag===0){
+function isSelect(e,mode){
+    if(mode==0){
+        if (flag===0){
+            new Audio(wSound[0]).play();
+            e.classList.add("js-white-checked","js-unclickable");
+            e.setAttribute('value',0);
+            setMessage("black-turn");
+            flag=1
+        }else if(flag===1){
+            new Audio(wSound[1]).play();
+            e.classList.add("js-black-checked","js-unclickable");
+            e.setAttribute('value',1);
+            setMessage("white-turn");
+            flag=0;
+        }
+        counter-=1;
+    }
+    else if(mode==1){
         new Audio(wSound[0]).play();
         e.classList.add("js-white-checked","js-unclickable");
         e.setAttribute('value',0);
-        setMessage("black-turn");
-        
-        flag=1
-    }else if(flag===1){
-        /*new Audio(wSound[1]).play();
-        e.classList.add("js-black-checked","js-unclickable");
-        e.setAttribute('value',1);
-        setMessage("white-turn");*/
-        
-        flag=0;
+        computer(1);
+        counter-=1;
     }
-    counter-=1;
 }
 
 function setMessage(id){
@@ -90,24 +97,25 @@ function setMessage(id){
 
 function newGame(){
     for(let i=0;i<square.length;i++){
-        square[i].classList.remove("js-white-checked","js-unclickable","js-black-checked");
+        square[i].classList.remove("js-white-checked","js-unclickable","js-black-checked","js-white_highLight","js-black_highLight");
     }
     counter=9;
     flag=0;
     value=new Array(9);
     whiteStorage=new Array();
     blackStorage=new Array();
+    lineStorage=new Array();
     randomStorage=[0,1,2,3,4,5,6,7,8];
     setMessage("white-turn");
     $(document).snowfall("clear");
 }
 
-function valueStorage(square,i){
-    value[i]=square[i].getAttribute('value');
-    if(value[i]==0){
+function value(square,i){
+    valueStorage[i]=square[i].getAttribute('value');
+    if(valueStorage[i]==0){
        whiteStorage.push(i);
     }
-    if(value[i]==1){
+    if(valueStorage[i]==1){
        blackStorage.push(i);
     }
 }
@@ -117,19 +125,30 @@ function checkWin(){
         if(win.every(Element=>whiteStorage.indexOf(Element)>-1)){
             setMessage("white-win");
             snowfall(0);
-            gameOver();
+            lineStorage=win;
+            gameOver("white");
         }
         if(win.every(Element=>blackStorage.indexOf(Element)>-1)){
             setMessage("black-win");
             snowfall(1);
-            gameOver();
+            lineStorage=win;
+            gameOver("black");
         }
     } 
 }
 
-function gameOver(){
+function gameOver(status){
     for(let i=0;i<square.length;i++){
         square[i].classList.add("js-unclickable");
+    }
+    if(status=="white"){
+        for(let [,n]of lineStorage.entries()){
+            square[n].classList.add("js-white_highLight");
+        }
+    }else if(status=="black"){
+        for(let [,n]of lineStorage.entries()){
+            square[n].classList.add("js-black_highLight");
+        }
     }
 }
 
@@ -147,18 +166,13 @@ function snowfall(n){
 
 function computer(mode){
     if(mode==1){
-        for(let [,value]of whiteStorage.entries()){
-            randomStorage.splice(value,1);
-        }
-        for(let [,value]of blackStorage.entries()){
-            randomStorage.splice(value,1);
-        }
-        n=Math.floor(Math.random()*randomStorage.length);
+        randomStorage=valueStorage.filter(function(index){
+           return index==valueStorage.indexOf("null");
+        })
+        console.log(randomStorage);
+        let n=Math.floor(Math.random()*randomStorage.length);
         new Audio(wSound[1]).play();
         square[randomStorage[n]].classList.add("js-black-checked","js-unclickable");
         square[randomStorage[n]].setAttribute('value',1);
-        setMessage("white-turn");
-        flag=0;
     }
 }
-
