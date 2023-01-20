d716532bca5661ee9f80966c9e6124dfaefe060e
aki9c <aki9c223@gmail.com>
2023-01-20 15:12:39 +0900

    test1

--- topPage/prod/tic_tac_toe/js/tic_tac_toe.js
+++ topPage/prod/tic_tac_toe/js/tic_tac_toe.js
@@ -2,7 +2,6 @@
 let flag=0; 
 let counter=9;
 let wSound=['sound/click_sound1.mp3','sound/click_sound2.mp3','sound/draw_sound.mp3','sound/white_sound.mp3','sound/black_sound.mp3'];
-let valueStorage=new Array(9);
 let whiteStorage=new Array();
 let blackStorage=new Array();
 let lineStorage=new Array();
@@ -16,7 +15,7 @@ const msgtxt4='<p class="image"><img src="img/black.png" width=61px height=61px>
 const msgtxt0='<p class="text"style="height:103px; font-size:48px;">Drow!</p>';
 const winCombos=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]];
 const image=['img/ParticleSmoke.png','img/star.png'];
-
+let s=Array.from(square);
 window.addEventListener("DOMContentLoaded",
     function() {
        setMessage("white-turn");
@@ -25,15 +24,15 @@ window.addEventListener("DOMContentLoaded",
 
 for(let i=0;i<square.length;i++){
     square[i].addEventListener("click",()=>{
-        isSelect(square[i],0);
-        valueCheck(square,i);
-        
+        isSelect(square[i],square,i,1);
+        valueCheck(square,i,1);
         checkWin();
         if(counter==0&&flag!=2){
             setMessage("draw");
             gameOver("draw");
         }
     })
+    
 }
 
 restart.addEventListener("click",()=>{
@@ -52,29 +51,13 @@ restart.addEventListener('click',e=>{
 
 function isSelect(e,mode){
     if(mode==0){
-        if (flag===0){
-            new Audio(wSound[0]).play();
-            e.classList.add("js-white-checked","js-unclickable");
-            e.setAttribute('value',0);
-            setMessage("black-turn");
-            flag=1
-        }else if(flag===1){
-            new Audio(wSound[1]).play();
-            e.classList.add("js-black-checked","js-unclickable");
-            e.setAttribute('value',1);
-            setMessage("white-turn");
-            flag=0;
-        }
-        counter-=1;
+       computer(e,mode);
     }
     else if(mode==1){
-        new Audio(wSound[0]).play();
-        e.classList.add("js-white-checked","js-unclickable");
-        e.setAttribute('value',0);
-        setMessage("black-turn");
-
+       computer(e,mode);
     }
 }
+
 
 function setMessage(id){
     const msgtext=document.getElementById("msgtext");
@@ -104,19 +87,24 @@ function newGame(){
     whiteStorage=new Array();
     blackStorage=new Array();
     lineStorage=new Array();
-    valueStorage=new Array(9);
-    randomStorage=new Array();
+    s=Array.from(square);
     setMessage("white-turn");
     $(document).snowfall("clear");
 }
 
-function valueCheck(square,i){
-    valueStorage[i]=square[i].getAttribute('value');
-    if(valueStorage[i]==0){
+function valueCheck(square,i,mode){
+    if(square[i].getAttribute('value')==0){
         whiteStorage.push(i);
     }
-    if(valueStorage[i]==1){
+    if(square[i].getAttribute('value')==1&&mode==0){
         blackStorage.push(i);
+    }
+    if(mode==1){
+        s.map((val,i)=>{
+         if(val.getAttribute('value')==1){
+            blackStorage.push(i);
+         }
+        })
     }
 }
 
@@ -164,18 +152,45 @@ function snowfall(n){
     })
 }
 
-function computer(square,mode){
-    if(mode==1){
-        for(let [n]of square.entries()){
-            if(square[n].getAttribute('value')==0);
+function computer(e,mode){
+    if(mode==0){
+        if (flag===0){
+            new Audio(wSound[0]).play();
+            e.classList.add("js-white-checked","js-unclickable");
+            e.setAttribute('value',0);
+            setMessage("black-turn");
+            flag=1
+        }else if(flag===1){
+            new Audio(wSound[1]).play();
+            e.classList.add("js-black-checked","js-unclickable");
+            e.setAttribute('value',1);
+            setMessage("white-turn");
+            flag=0;
         }
-        let n=Math.floor(Math.random()*randomStorage.length);
-        new Audio(wSound[1]).play();
-        square[randomStorage[n]].classList.add("js-black-checked","js-unclickable");
-        square[randomStorage[n]].setAttribute('value',1);
-        setMessage("white-turn");
-        flag=0;
-        console.log(randomStorage);
-        console.log(randomStorage.length);
+        counter-=1;
+    }else if(mode==1){
+            new Audio(wSound[0]).play();
+            e.classList.add("js-white-checked","js-unclickable");
+            e.setAttribute('value',0);
+            setMessage("black-turn");
+            
+            emptyIndexies(s);
+            console.log(emptyIndexies(s));
+            let n=Math.floor(emptyIndexies(s).length*Math.random());
+            if(counter>1){
+                emptyIndexies(s)[n].classList.add("js-black-checked","js-unclickable");
+                emptyIndexies(s)[n].setAttribute('value',1);
+            }
+            counter-=2;
+            setTimeout(function(){
+            },2000)
+           
     }
+    
 }
+
+function emptyIndexies(board){
+    return board.filter(val=>val.getAttribute('value')!=0&&val.getAttribute('value')!=1);
+}
+
+
